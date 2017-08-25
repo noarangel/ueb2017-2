96a8339c2599d8edbc6a24cc09c758e7870ff7c4
ilgaleanos <ilgaleanos@gmail.com>
2017-08-25 00:34:53 -0500

    MCM y primo

--- ProgramaDiscretas/src/controller/DarthVader.java
+++ ProgramaDiscretas/src/controller/DarthVader.java
@@ -64,4 +64,65 @@ public class DarthVader {
 		return entero;
 
 	}
+
+	public int getDivisor(int numero) {
+		if (numero % 2 == 0) {
+			return 2;
+		}
+
+		double raiz = Math.sqrt(numero);
+		for (int i = 3; i < raiz; i += 2) {
+			if (numero % i == 0) {
+				return i;
+			}
+		}
+
+		return 1;
+	}
+
+	public boolean areAllOne(int[] numeros) {
+		for (int i = 0; i < numeros.length; i++) {
+			if (numeros[i] != 1) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	public int minimoComunMultiplo(int[] numeros) {
+
+		int divisor;
+		int MCM = 1;
+
+		while (areAllOne(numeros) == false) {
+
+			for (int i = 0; i < numeros.length; i++) {
+				divisor = getDivisor(numeros[i]);
+
+				if (divisor != 1) {
+
+					for (int k = 0; k < numeros.length; k++) {
+						numeros[k] = (numeros[k] % divisor == 0) ? (int) (numeros[k] / divisor) : numeros[k];
+					}
+
+					MCM *= divisor;
+				}
+
+				else if (divisor == 1) {
+					divisor = numeros[i];
+
+					for (int k = 0; k < numeros.length; k++) {
+						numeros[k] = (numeros[k] % divisor == 0) ? (int) (numeros[k] / divisor) : numeros[k];
+					}
+
+					MCM *= divisor;
+				}
+
+			}
+
+		}
+
+		return MCM;
+	}
 }
